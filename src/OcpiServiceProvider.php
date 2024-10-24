ef289732eb0ba812552e0350f28b94a96f9f198f
agnonym <contact@agnonym.com>
2024-10-24 11:52:24 +0200

    + | Provider - Ocpi

---
+++ src/OcpiServiceProvider.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Ocpi;
+
+use Illuminate\Support\Facades\URL;
+use Illuminate\Support\ServiceProvider;
+
+class OcpiServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
+    public function boot(): void
+    {
+    }
+}
