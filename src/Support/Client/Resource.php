a515b23fbc54fa1673a179a4853ab01f3c3a2cbe
agnonym <contact@agnonym.com>
2024-10-28 15:13:49 +0100

    + | Resource - Support - Client - Resource

---
+++ src/Support/Client/Resource.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Ocpi\Support\Client;
+
+use Ocpi\Support\Client\Requests\GetRequest;
+use Ocpi\Support\Client\Requests\PostRequest;
+use Saloon\Http\BaseResource;
+use Saloon\Http\Response;
+
+class Resource extends BaseResource
+{
+    public function requestGetSend(): ?array
+    {
+        $response = $this->connector->send(new GetRequest);
+
+        $response->throw();
+
+        return $this->responseGetProcess($response);
+    }
+
+    public function requestPostSend(?array $payload): ?array
+    {
+        $response = $this->connector->send(new PostRequest($payload));
+
+        $response->throw();
+
+        return $this->responsePostProcess($response);
+    }
+
+    public function responseGetProcess(Response $response): ?array
+    {
+        if (! $response->successful()) {
+            return null;
+        }
+
+        $responseObject = $response->object();
+
+        return ($responseObject?->data ?? null) ? (array) $responseObject->data : null;
+    }
+
+    public function responsePostProcess(Response $response): ?array
+    {
+        if (! $response->successful()) {
+            return null;
+        }
+
+        $responseObject = $response->object();
+
+        return ($responseObject?->data ?? null) ? (array) $responseObject->data : null;
+    }
+}
