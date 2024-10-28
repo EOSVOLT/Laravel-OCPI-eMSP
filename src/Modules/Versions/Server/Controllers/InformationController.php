164b04af136f284f6cd9b0ad4e548b4a9e3a7b94
agnonym <contact@agnonym.com>
2024-10-28 15:45:44 +0100

    + | Module - Versions

---
+++ src/Modules/Versions/Server/Controllers/InformationController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Ocpi\Modules\Versions\Server\Controllers;
+
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Str;
+use Ocpi\Support\Server\Controllers\Controller;
+
+class InformationController extends Controller
+{
+    public function __invoke(Request $request): JsonResponse
+    {
+        $data = collect(config('ocpi-emsp.versions', []))
+            ->map(function ($moduleList, $version) {
+                $route = 'ocpi.emsp.'.Str::replace('.', '_', $version).'.versions.details';
+
+                return Route::has($route)
+                    ? [
+                        'version' => $version,
+                        'url' => route($route),
+                    ]
+                    : null;
+            })
+            ->filter()
+            ->values()
+            ->toArray();
+
+        return $this->ocpiSuccessResponse($data);
+    }
+}
