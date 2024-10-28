bfd022ac1f32fdd4157f229ca5d355caf8afe46b
agnonym <contact@agnonym.com>
2024-10-28 15:08:39 +0100

    + | Middleware - Support - Client - LogResponse

---
+++ src/Support/Client/Middlewares/LogResponse.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Ocpi\Support\Client\Middlewares;
+
+use Illuminate\Support\Facades\Log;
+use Saloon\Contracts\ResponseMiddleware;
+use Saloon\Http\Response;
+
+class LogResponse implements ResponseMiddleware
+{
+    public function __invoke(Response $response): void
+    {
+        if ($response->failed()) {
+            $body = $response->getPendingRequest()->body()?->isNotEmpty()
+                ? json_encode($response->getPendingRequest()->body()->all(), JSON_PRETTY_PRINT | JSON_UNESCAPED_SLASHES)
+                : null;
+
+            Log::channel('ocpi')->error(
+                '[OUT] '.$response->getPendingRequest()->getMethod()->value.' '
+                .$response->getPendingRequest()->getUrl()
+                .($body ? PHP_EOL.$body : '')
+            );
+            Log::channel('ocpi')->error($response->toException()->getMessage());
+            Log::channel('ocpi')->error($response->toException()->getTraceAsString());
+        }
+    }
+}
