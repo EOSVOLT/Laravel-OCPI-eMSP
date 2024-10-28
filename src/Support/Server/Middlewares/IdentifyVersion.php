928d67f4862644b647940dea5187040dbe2ab3cc
agnonym <contact@agnonym.com>
2024-10-28 14:39:03 +0100

    + | Middleware - Support - Server - IdentifyVersion: extract OCPI version from URL

---
+++ src/Support/Server/Middlewares/IdentifyVersion.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Ocpi\Support\Server\Middlewares;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Context;
+use Illuminate\Support\Str;
+use Symfony\Component\HttpFoundation\Response;
+
+class IdentifyVersion
+{
+    public function handle(Request $request, Closure $next): Response
+    {
+        $path = Str::before(Str::after($request->path(), 'ocpi/emsp/'), '/');
+
+        if (Str::contains($path, '.')) {
+            Context::add('ocpi_version', $path);
+        }
+
+        return $next($request);
+    }
+}
