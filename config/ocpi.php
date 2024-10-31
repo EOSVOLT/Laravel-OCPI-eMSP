1f91050bc37b571798bff472b3ad5a893dc7ab40
agnonym <contact@agnonym.com>
2024-10-31 15:35:00 +0100

    § | Config - Ocpi: add default value for database.connection

--- config/ocpi.php
+++ config/ocpi.php
@@ -9,7 +9,7 @@ return [
     */
 
     'database' => [
-        'connection' => env('OCPI_DATABASE_CONNECTION'),
+        'connection' => env('OCPI_DATABASE_CONNECTION', env('DB_CONNECTION', 'sqlite')),
         'table' => [
             'prefix' => env('OCPI_DATABASE_TABLE_PREFIX', 'ocpi_'),
         ],
