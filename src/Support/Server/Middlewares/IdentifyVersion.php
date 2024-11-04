a7464c3c0eabfe878d5c05bab48d515832f1eb02
agnonym <contact@agnonym.com>
2024-11-04 12:14:48 +0100

    * | Middleware - Support - Server - IdentifyParty: use uri prefix from config

--- src/Support/Server/Middlewares/IdentifyVersion.php
+++ src/Support/Server/Middlewares/IdentifyVersion.php
@@ -12,7 +12,7 @@ class IdentifyVersion
 {
     public function handle(Request $request, Closure $next): Response
     {
-        $path = Str::before(Str::after($request->path(), 'ocpi/emsp/'), '/');
+        $path = Str::before(Str::after($request->path(), config('ocpi.server.routing.uri_prefix').'/'), '/');
 
         if (Str::contains($path, '.')) {
             Context::add('ocpi_version', $path);
