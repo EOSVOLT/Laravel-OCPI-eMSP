508065ec0fb724b0cf390df79884bf2561762f98
agnonym <contact@agnonym.com>
2025-01-15 18:45:17 +0100

    + | Model - Commands - Command

---
+++ src/Data/Migrations/2025_01_15_create_ocpi_commands_table.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Get the migration connection name.
+     */
+    public function getConnection(): ?string
+    {
+        return config('ocpi.database.connection');
+    }
+
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create(config('ocpi.database.table.prefix').'commands', function (Blueprint $table) {
+            $table->ulid('id')->primary();
+            $table->foreignId('party_role_id')
+                ->constrained(
+                    table: config('ocpi.database.table.prefix').'party_roles',
+                    indexName: 'commands_party_role_id',
+                )
+                ->cascadeOnDelete();
+            $table->string('type', length: 20);
+            $table->json('payload')->nullable();
+            $table->string('response', length: 15)->nullable();
+            $table->string('result', length: 20)->nullable();
+            $table->timestamps();
+
+            $table->index('response');
+            $table->index('result');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists(config('ocpi.database.table.prefix').'commands');
+    }
+};
---
+++ src/Models/Commands/Command.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Ocpi\Models\Commands;
+
+use Illuminate\Database\Eloquent\Casts\AsArrayObject;
+use Illuminate\Database\Eloquent\Concerns\HasUlids;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Ocpi\Models\Commands\Enums\CommandResponseType;
+use Ocpi\Models\Commands\Enums\CommandResultType;
+use Ocpi\Models\Commands\Enums\CommandType;
+use Ocpi\Models\PartyRole;
+use Ocpi\Support\Models\Model;
+
+class Command extends Model
+{
+    use HasUlids;
+
+    protected $fillable = [
+        'id',
+        'party_role_id',
+        'type',
+        'payload',
+        'response',
+        'result',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'type' => CommandType::class,
+            'payload' => AsArrayObject::class,
+            'response' => CommandResponseType::class,
+            'result' => CommandResultType::class,
+        ];
+    }
+
+    /***
+     * Relations.
+     ***/
+
+    public function party_role(): BelongsTo
+    {
+        return $this->belongsTo(PartyRole::class);
+    }
+}
