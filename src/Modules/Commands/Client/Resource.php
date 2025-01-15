90752eb5189d7a84304223ace664066eb07b64b4
agnonym <contact@agnonym.com>
2025-01-15 19:00:19 +0100

    + | Module - Commands: WIP with Client reserveNow, Server Post

---
+++ src/Modules/Commands/Client/Resource.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Ocpi\Modules\Commands\Client;
+
+use Exception;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+use Log;
+use Ocpi\Models\Commands\Command;
+use Ocpi\Models\Commands\Enums\CommandResponseType;
+use Ocpi\Models\Commands\Enums\CommandType;
+use Ocpi\Models\PartyRole;
+use Ocpi\Support\Client\Resource as OcpiResource;
+
+class Resource extends OcpiResource
+{
+    public function reserveNow(PartyRole $partyRole, array $payload): void
+    {
+        DB::beginTransaction();
+        try {
+            $command = Command::create([
+                'party_role_id' => $partyRole->id,
+                'type' => CommandType::RESERVE_NOW,
+            ]);
+
+            $payload['response_url'] = $this->responseUrl($partyRole, $command);
+
+            $command->payload = $payload;
+            $command->save();
+
+            DB::commit();
+
+            $response = $this->requestPostSend(
+                payload: $command->payload->toArray(),
+                endpoint: $command->type->name,
+            );
+
+            $commandResponseType = CommandResponseType::fromName($response);
+            if (! $commandResponseType) {
+                Log::channel('ocpi')->error('Unknown CommandResponseType '.json_encode($response));
+                throw new Exception('Unknown CommandResponseType '.json_encode($response));
+            }
+
+            $command->response = $commandResponseType->name;
+            $command->save();
+
+            if ($commandResponseType !== CommandResponseType::ACCEPTED) {
+                throw new Exception('Command not accepted');
+            }
+        } catch (Exception $e) {
+            DB::rollBack();
+            throw $e;
+        }
+    }
+
+    private function responseUrl(PartyRole $partyRole, Command $command): string
+    {
+        return (config('ocpi.server.enabled', false) === true)
+            ? route('ocpi.emsp.'.Str::replace('.', '_', $partyRole?->party?->version).'.commands.post', [
+                'command' => $command->type->name,
+                'id' => $command->id,
+            ])
+            : config('ocpi.client.server.url').'/'.$partyRole?->party?->version.'/commands/'.$command->type->name.'/'.$command->id;
+    }
+}
