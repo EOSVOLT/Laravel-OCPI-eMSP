25707fa4e8836845fad95bfc49b3de677b483c3a
agnonym <contact@agnonym.com>
2025-02-06 22:30:11 +0100

    = | Action - Module - Versions - Party - PartyInformationAndDetailsSynchronize: cleaning

--- src/Modules/Versions/Actions/PartyInformationAndDetailsSynchronizeAction.php
+++ src/Modules/Versions/Actions/PartyInformationAndDetailsSynchronizeAction.php
@@ -13,12 +13,12 @@ class PartyInformationAndDetailsSynchronizeAction
     public function handle(Party $party): Party
     {
         // OCPI GET call for Versions Information of the Party, store OCPI version and URL.
-        Log::channel('ocpi')->info('['.$party->code.'] OCPI GET call for Versions Information of the Party on '.$party->url);
+        Log::channel('ocpi')->info('Party '.$party->code.' - OCPI GET call for Versions Information of the Party on '.$party->url);
         $ocpiClient = new OcpiClient($party, 'versions.information');
         $versionList = $ocpiClient->versions()->information();
         throw_if(
             ! is_array($versionList),
-            new Exception('['.$party->code.'] Empty or invalid response for Versions Information.')
+            new Exception('Party '.$party->code.' - Empty or invalid response for Versions Information.')
         );
 
         // Find most recent version.
@@ -30,43 +30,43 @@ class PartyInformationAndDetailsSynchronizeAction
         }
         throw_if(
             $currentItem === null || ! Arr::has($currentItem, ['version', 'url']),
-            new Exception('['.$party->code.'] No version found.')
+            new Exception('Party '.$party->code.' - No version found.')
         );
 
-        Log::channel('ocpi')->info('['.$party->code.'] Set Party OCPI version to '.$currentItem['version']);
+        Log::channel('ocpi')->info('Party '.$party->code.' - Set Party OCPI version to '.$currentItem['version']);
         $party->version = $currentItem['version'];
         $party->version_url = $currentItem['url'];
         throw_if(
             ! $party->save(),
-            new Exception('['.$party->code.'] Error updating Party OCPI version.')
+            new Exception('Party '.$party->code.' - Error updating Party OCPI version.')
         );
 
         // OCPI GET call for Versions Details of the Party, store OCPI endpoints.
-        Log::channel('ocpi')->info('['.$party->code.'] OCPI GET call for Versions Details of the Party for version '.$party->version);
+        Log::channel('ocpi')->info('Party '.$party->code.' - OCPI GET call for Versions Details of the Party for version '.$party->version);
         $ocpiClient->module('versions.details');
         $versionDetails = $ocpiClient->versions()->details();
         throw_if(
             ! is_array($versionDetails) || ! isset($versionDetails['version']) || ! is_array($versionDetails['endpoints'] ?? null),
-            new Exception('['.$party->code.'] Empty or invalid response for Versions Details.')
+            new Exception('Party '.$party->code.' - Empty or invalid response for Versions Details.')
         );
         throw_if(
             $versionDetails['version'] !== $party->version,
-            new Exception('['.$party->code.'] Version mismatch for Versions Details: requested '.$party->version.' / received '.$versionDetails['version'].'.')
+            new Exception('Party '.$party->code.' - Version mismatch for Versions Details: requested '.$party->version.' / received '.$versionDetails['version'].'.')
         );
 
         // Set Party OCPI endpoints for version.
-        Log::channel('ocpi')->info('['.$party->code.'] Set OCPI endpoints for version '.$party->version);
+        Log::channel('ocpi')->info('Party '.$party->code.' - Set OCPI endpoints for version '.$party->version);
         $party->endpoints = collect($versionDetails['endpoints'])
             ->pluck('url', 'identifier')
             ->toArray();
         throw_if(
             ! Arr::has($party->endpoints, 'credentials'),
-            new Exception('['.$party->code.'] Missing required `credentials` Module endpoint.')
+            new Exception('Party '.$party->code.' - Missing required `credentials` Module endpoint.')
         );
 
         throw_if(
             ! $party->save(),
-            new Exception('['.$party->code.'] Error updating Party OCPI endpoints.')
+            new Exception('Party '.$party->code.' - Error updating Party OCPI endpoints.')
         );
 
         return $party;
