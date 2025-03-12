3f05f75094a72376a5cb21a922b19c9e0a3857c5
agnonym <contact@agnonym.com>
2025-03-12 18:33:28 +0100

    * | Resource - Module - Commands - Client: WIP add cancelReservation()

--- src/Modules/Commands/Client/Resource.php
+++ src/Modules/Commands/Client/Resource.php
@@ -17,11 +17,56 @@ class Resource extends OcpiResource
 {
     public function reserveNow(PartyRole $partyRole, array $payload): void
     {
-        DB::connection(config('ocpi.database.connection'))->beginTransaction();
         try {
+            DB::connection(config('ocpi.database.connection'))->beginTransaction();
+
             $command = Command::create([
                 'party_role_id' => $partyRole->id,
                 'type' => CommandType::RESERVE_NOW,
+            ]);
+
+            $payload['response_url'] = $this->responseUrl($partyRole, $command);
+
+            $command->payload = $payload;
+            $command->save();
+
+            DB::connection(config('ocpi.database.connection'))->commit();
+
+            $response = $this->requestPostSend(
+                payload: $command->payload->toArray(),
+                endpoint: $command->type->name,
+            );
+
+            $commandResponseType = CommandResponseType::fromName($response);
+            if (! $commandResponseType) {
+                Log::channel('ocpi')->error('Unknown CommandResponseType '.json_encode($response));
+                throw new Exception('Unknown CommandResponseType '.json_encode($response));
+            }
+
+            $command->response = $commandResponseType->name;
+            $command->save();
+
+            if ($commandResponseType === CommandResponseType::ACCEPTED) {
+                Events\CommandResponseAccepted::dispatch($partyRole->id, $command->id, $command->type->name);
+            } else {
+                Events\CommandResponseError::dispatch($partyRole->id, $command->id, $command->type->name, $payload);
+
+                throw new Exception('Command not accepted');
+            }
+        } catch (Exception $e) {
+            DB::connection(config('ocpi.database.connection'))->rollBack();
+            throw $e;
+        }
+    }
+
+    public function cancelReservation(PartyRole $partyRole, array $payload): void
+    {
+        try {
+            DB::connection(config('ocpi.database.connection'))->beginTransaction();
+
+            $command = Command::create([
+                'party_role_id' => $partyRole->id,
+                'type' => CommandType::CANCEL_RESERVATION,
             ]);
 
             $payload['response_url'] = $this->responseUrl($partyRole, $command);
