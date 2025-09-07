dbcb34cb9498b24fb5945c71394828cbbaa4738a
waritthon thanomsap <wt@eosvolt.com>
2025-09-08 05:19:54 +0700

    update type collection

--- src/Helpers/TypeCollection.php
+++ src/Helpers/TypeCollection.php
@@ -2,10 +2,11 @@
 
 namespace Ocpi\Helpers;
 
+use Illuminate\Contracts\Support\Arrayable;
 use Illuminate\Support\Collection;
 use InvalidArgumentException;
 
-abstract class TypeCollection extends Collection
+abstract class TypeCollection extends \ArrayIterator implements Arrayable
 {
     protected string $type;
 
@@ -15,14 +16,14 @@ abstract class TypeCollection extends Collection
             throw new InvalidArgumentException(sprintf('Collection type for %s is not specified!', get_class($this)));
         }
 
-        parent::add($item);
+        parent::append($item);
     }
 
     public function pluck($value, $key = null): array
     {
         $return = [];
         $camelValue = $this->snakeToCamel($value);
-        foreach ($this->items as $item) {
+        foreach ($this->getArrayCopy() as $item) {
             if (is_null($key)) {
                 $return[] = $item->{'get' . ucfirst($camelValue)}();
             } else {
@@ -35,5 +36,15 @@ abstract class TypeCollection extends Collection
     private function snakeToCamel(string $value): string
     {
         return str_replace('_', '', ucwords($value, '_'));
+    }
+
+    public function merge(Arrayable $collection): static
+    {
+        if (get_class($this) !== get_class($collection)) {
+            throw new InvalidArgumentException('Collections must be of the same type to merge.');
+        }
+
+        $merged = array_merge($this->getArrayCopy(), $collection->getArrayCopy());
+        return new static($merged);
     }
 }
