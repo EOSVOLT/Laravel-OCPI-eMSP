c8e68c182539a8770bceb1a7f432f38f3a223a98
waritthon thanomsap <wt@eosvolt.com>
2025-09-10 14:41:55 +0700

    update migration

--- src/Data/Migrations/2025_09_10_035122_alter_location_evse_connector_table.php
+++ src/Data/Migrations/2025_09_10_035122_alter_location_evse_connector_table.php
@@ -7,35 +7,11 @@ use Illuminate\Support\Facades\Schema;
 return new class extends Migration {
     public function up(): void
     {
-        Schema::table('ocpi_locations', function (Blueprint $table) {
-            $table->renameColumn('id', 'external_id');
-            $table->renameColumn('emsp_id', 'id');
-        });
-        Schema::table('ocpi_location_evses', function (Blueprint $table) {
-            $table->renameColumn('emsp_id', 'id');
-            $table->renameColumn('location_emsp_id', 'location_id');
-        });
-        Schema::table('ocpi_location_connectors', function (Blueprint $table) {
-            $table->renameColumn('id', 'connector_id');
-            $table->renameColumn('emsp_id', 'id');
-            $table->renameColumn('location_evse_emsp_id', 'evse_id');
-        });
+
     }
 
     public function down(): void
     {
-        Schema::table('ocpi_location_connectors', function (Blueprint $table) {
-            $table->renameColumn('id', 'emsp_id');
-            $table->renameColumn('connector_id', 'id');
-            $table->renameColumn('evse_id', 'location_evse_emsp_id');
-        });
-        Schema::table('ocpi_location_evses', function (Blueprint $table) {
-            $table->renameColumn('id', 'emsp_id');
-            $table->renameColumn('location_id', 'location_emsp_id');
-        });
-        Schema::table('ocpi_locations', function (Blueprint $table) {
-            $table->renameColumn('id', 'emsp_id');
-            $table->renameColumn('external_id', 'id');
-        });
+
     }
 };
