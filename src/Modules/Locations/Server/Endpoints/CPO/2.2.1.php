827f85454636fe6e6793f984fe30c3eb09f52fb1
waritthon thanomsap <wt@eosvolt.com>
2025-09-17 16:31:53 +0700

    create cpo location api

--- src/Modules/Locations/Server/Endpoints/CPO/2.2.1.php
+++ src/Modules/Locations/Server/Endpoints/CPO/2.2.1.php
@@ -2,17 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 use Ocpi\Modules\Locations\Server\Controllers\CPO\GetController;
-use Ocpi\Modules\Locations\Server\Controllers\CPO\PatchController;
-use Ocpi\Modules\Locations\Server\Controllers\CPO\PutController;
-use Ocpi\Support\Server\Middlewares\IdentifyPartyRole;
+use Ocpi\Support\Server\Middlewares\IdentifyCPOSender;
 
 Route::middleware([
-    IdentifyPartyRole::class,
+    IdentifyCPOSender::class,
 ])
     ->prefix('locations')
     ->name('locations')
     ->group(function () {
-        Route::get('{country_code?}/{party_id?}/{location_id?}/{evse_uid?}/{connector_id?}', GetController::class);
-        Route::put('{country_code}/{party_id}/{location_id}/{evse_uid?}/{connector_id?}', PutController::class)->name('.put');
-        Route::patch('{country_code}/{party_id}/{location_id}/{evse_uid?}/{connector_id?}', PatchController::class)->name('.patch');
+        Route::get('', GetController::class);
     });
---
+++ src/Support/Server/Middlewares/IdentifyCPOSender.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Ocpi\Support\Server\Middlewares;
+
+class IdentifyCPOSender extends IdentifySenderParty
+{
+
+}
