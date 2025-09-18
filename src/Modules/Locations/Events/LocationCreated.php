77e5bb0704fa57ee739a154d280520bb09f14ef2
waritthon thanomsap <wt@eosvolt.com>
2025-09-18 20:36:57 +0700

    update resource param

--- src/Modules/Locations/Events/LocationCreated.php
+++ src/Modules/Locations/Events/LocationCreated.php
@@ -5,16 +5,17 @@ namespace Ocpi\Modules\Locations\Events;
 use Illuminate\Contracts\Events\ShouldDispatchAfterCommit;
 use Illuminate\Foundation\Events\Dispatchable;
 use Ocpi\Models\Locations\Location;
+use Ocpi\Modules\Locations\Objects\Locations;
 
 class LocationCreated implements ShouldDispatchAfterCommit
 {
     use Dispatchable;
 
     public function __construct(
-        private readonly Location $location,
+        private readonly Locations $location,
     ) {}
 
-    public function getLocation(): Location
+    public function getLocation(): Locations
     {
         return $this->location;
     }
--- src/Modules/Locations/Events/LocationRestored.php
+++ src/Modules/Locations/Events/LocationRestored.php
@@ -4,17 +4,18 @@ namespace Ocpi\Modules\Locations\Events;
 
 use Illuminate\Contracts\Events\ShouldDispatchAfterCommit;
 use Illuminate\Foundation\Events\Dispatchable;
-use Ocpi\Models\Locations\Location;
+use Ocpi\Modules\Locations\Objects\Locations;
 
 class LocationRestored implements ShouldDispatchAfterCommit
 {
     use Dispatchable;
 
     public function __construct(
-        private readonly Location $location,
-    ) {}
+        private readonly Locations $location,
+    ) {
+    }
 
-    public function getLocation(): Location
+    public function getLocation(): Locations
     {
         return $this->location;
     }
