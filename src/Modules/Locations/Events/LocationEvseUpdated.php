9ef13b1925a6c23385078cc5cc595819c6260512
waritthon thanomsap <wt@eosvolt.com>
2025-09-18 21:26:54 +0700

    update resource param

--- src/Modules/Locations/Events/LocationEvseUpdated.php
+++ src/Modules/Locations/Events/LocationEvseUpdated.php
@@ -5,17 +5,24 @@ namespace Ocpi\Modules\Locations\Events;
 use Illuminate\Contracts\Events\ShouldDispatchAfterCommit;
 use Illuminate\Foundation\Events\Dispatchable;
 use Ocpi\Modules\Locations\Objects\Evse;
+use Ocpi\Modules\Locations\Objects\Locations;
 
 class LocationEvseUpdated implements ShouldDispatchAfterCommit
 {
     use Dispatchable;
 
     public function __construct(
+        private readonly Locations $location,
         private readonly Evse $evse,
     ) {}
 
     public function getEvse(): Evse
     {
         return $this->evse;
+    }
+
+    public function getLocation(): Locations
+    {
+        return $this->location;
     }
 }
