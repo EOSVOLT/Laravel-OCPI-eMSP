6f82c5f5afb31e0158e5e5539c74f85058dfff58
chanapon <mike0156@hotmail.com>
2025-09-22 15:21:28 +0700

    [EOS-2441] start debug the request

--- src/Modules/Commands/Server/Controllers/CPO/V2_2_1/PostController.php
+++ src/Modules/Commands/Server/Controllers/CPO/V2_2_1/PostController.php
@@ -8,50 +8,50 @@ use Illuminate\Http\Request;
 use Log;
 use Ocpi\Models\Commands\Command;
 use Ocpi\Models\Commands\Enums\CommandResultType;
+use Ocpi\Models\Commands\Enums\CommandType;
 use Ocpi\Modules\Commands\Events;
 use Ocpi\Support\Enums\OcpiClientErrorCode;
 use Ocpi\Support\Server\Controllers\Controller;
 
 class PostController extends Controller
 {
-    public function __invoke(
-        Request $request,
-        string $type,
-        ?string $id = null,
-    ): JsonResponse {
+    public function __invoke(Request $request, CommandType $command): JsonResponse {
         try {
-            $command = Command::query()
-                ->with(['party_role'])
-                ->where('id', $id)
-                ->where('type', $type)
-                ->first();
+            $allRequest = $request->toArray();
+//            $command = Command::query()->create([
+//
+//            ]);
+//                ->with(['party_role'])
+//                ->where('id', $id)
+//                ->where('type', $type)
+//                ->first();
+//
+//            if (! $command) {
+//                Log::channel('ocpi')->error('Unknown Command '.$type.':'.$id);
+//
+//                return $this->ocpiClientErrorResponse(
+//                    statusCode: OcpiClientErrorCode::InvalidParameters,
+//                    statusMessage: 'Unknown Command.',
+//                );
+//            }
+//
+//            $result = $request->input('result');
+//            $commandResultType = CommandResultType::fromName($result);
+//            if (! $commandResultType) {
+//                throw new Exception('Unknown CommandResultType '.$result.' for Command '.$type.':'.$id);
+//            }
+//
+//            $command->result = $commandResultType->name;
+//            $command->save();
 
-            if (! $command) {
-                Log::channel('ocpi')->error('Unknown Command '.$type.':'.$id);
-
-                return $this->ocpiClientErrorResponse(
-                    statusCode: OcpiClientErrorCode::InvalidParameters,
-                    statusMessage: 'Unknown Command.',
-                );
-            }
-
-            $result = $request->input('result');
-            $commandResultType = CommandResultType::fromName($result);
-            if (! $commandResultType) {
-                throw new Exception('Unknown CommandResultType '.$result.' for Command '.$type.':'.$id);
-            }
-
-            $command->result = $commandResultType->name;
-            $command->save();
-
-            if (
-                $commandResultType === CommandResultType::ACCEPTED
-                || $commandResultType === CommandResultType::CANCELED_RESERVATION
-            ) {
-                Events\CommandResultSucceeded::dispatch($command->party_role->id, $command->id, $command->type->name);
-            } else {
-                Events\CommandResultError::dispatch($command->party_role->id, $command->id, $command->type->name, $command->payload);
-            }
+//            if (
+//                $commandResultType === CommandResultType::ACCEPTED
+//                || $commandResultType === CommandResultType::CANCELED_RESERVATION
+//            ) {
+//                Events\CommandResultSucceeded::dispatch($command->party_role->id, $command->id, $command->type->name);
+//            } else {
+//                Events\CommandResultError::dispatch($command->party_role->id, $command->id, $command->type->name, $command->payload);
+//            }
 
             return $this->ocpiSuccessResponse();
         } catch (Exception $e) {
