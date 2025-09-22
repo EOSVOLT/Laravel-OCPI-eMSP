8db6e9b7380afc4a8102fd5af6ab425c8f7d531d
waritthon thanomsap <wt@eosvolt.com>
2025-09-22 16:05:46 +0700

    rename emsp client

--- src/Modules/Locations/Client/V2_2_1/EMSPClient.php
+++ src/Modules/Locations/Client/V2_2_1/EMSPClient.php
@@ -2,18 +2,9 @@
 
 namespace Ocpi\Modules\Locations\Client\V2_2_1;
 
-use Ocpi\Modules\Locations\Traits\HandlesLocation;
 use Ocpi\Support\Client\ReceiverClient;
 
 class EMSPClient extends ReceiverClient
 {
-    use handlesLocation;
 
-    /**
-     * @return string
-     */
-    public function resolveBaseUrl(): string
-    {
-        return parent::resolveBaseUrl(). self::getLocationPath('2.2.1');
-    }
 }
