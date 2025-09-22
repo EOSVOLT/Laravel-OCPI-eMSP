2692ffd0612d92608186cafc6a4175309ac7392d
waritthon thanomsap <wt@eosvolt.com>
2025-09-22 15:46:40 +0700

    rename emsp client

--- src/Modules/Locations/Client/V2_2_1/EMSPClient.php
+++ src/Modules/Locations/Client/V2_2_1/EMSPClient.php
@@ -2,11 +2,10 @@
 
 namespace Ocpi\Modules\Locations\Client\V2_2_1;
 
-use Ocpi\Modules\Locations\Client\V2_2_1\Resource as LocationsResource;
 use Ocpi\Modules\Locations\Traits\HandlesLocation;
 use Ocpi\Support\Client\SenderClient;
 
-class CPOClient extends SenderClient
+class EMSPClient extends SenderClient
 {
     use handlesLocation;
 }
