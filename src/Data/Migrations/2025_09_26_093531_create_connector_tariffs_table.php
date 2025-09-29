f470c8dba7b5e5ee079e6948705ba7b19ebda7bd
waritthon thanomsap <wt@eosvolt.com>
2025-09-29 16:51:27 +0700

    fix migration

--- src/Data/Migrations/2025_09_26_093531_create_connector_tariffs_table.php
+++ src/Data/Migrations/2025_09_26_093531_create_connector_tariffs_table.php
@@ -7,14 +7,28 @@ use Illuminate\Support\Facades\Schema;
 return new class extends Migration {
     public function up(): void
     {
-        Schema::create(config('ocpi.database.table.prefix').'location_connector_tariffs', function (Blueprint $table) {
-            $table->foreign('connector_id')->references('id')->on(config('ocpi.database.table.prefix') . 'location_connectors');
-            $table->foreign('tariff_id')->references('id')->on(config('ocpi.database.table.prefix') . 'tariffs');
-        });
+        Schema::create(
+            config('ocpi.database.table.prefix') . 'location_connector_tariffs',
+            function (Blueprint $table) {
+                $table->foreignId('connector_id')->constrained(
+                    config('ocpi.database.table.prefix') . 'location_connectors',
+                    indexName: 'location_connector_tariffs_connector_id',
+                )->onDelete('cascade');
+                $table->foreignId('tariff_id')->constrained(
+                    config('ocpi.database.table.prefix') . 'tariffs',
+                    indexName: 'location_connector_tariffs_tariff_id',
+                )->onDelete(
+                    'cascade'
+                );
+                $table->unique(['connector_id', 'tariff_id']);
+            }
+        );
     }
 
     public function down(): void
     {
-        Schema::dropIfExists('connector_tariffs');
+        Schema::disableForeignKeyConstraints();
+        Schema::dropIfExists(config('ocpi.database.table.prefix') . 'location_connector_tariffs');
+        Schema::enableForeignKeyConstraints();
     }
 };
