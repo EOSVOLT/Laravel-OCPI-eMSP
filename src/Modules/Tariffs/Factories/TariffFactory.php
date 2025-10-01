f7b71279feef651eb324991e05e59b63739e3187
waritthon thanomsap <wt@eosvolt.com>
2025-10-01 10:41:07 +0700

    fix tariff factory

--- src/Modules/Tariffs/Factories/TariffFactory.php
+++ src/Modules/Tariffs/Factories/TariffFactory.php
@@ -5,23 +5,27 @@ namespace Ocpi\Modules\Tariffs\Factories;
 use Illuminate\Support\Collection;
 use Ocpi\Models\Tariff\Tariff;
 use Ocpi\Modules\Tariffs\Objects\TariffCollection;
-use Ocpi\Support\Enums\Role;
 
 class TariffFactory
 {
     public static function fromModel(Tariff $model): \Ocpi\Modules\Tariffs\Objects\Tariff
     {
         $role = $model->party->role_cpo;
-        return new \Ocpi\Modules\Tariffs\Objects\Tariff(
+        $tariff = new \Ocpi\Modules\Tariffs\Objects\Tariff(
             $role->country_code,
             $role->code,
             $model->external_id,
             $model->currency,
             TariffElementFactory::fromCollection($model->elements),
             $model->updated_at
-        )
-            ->setMinPrice(PriceFactory::fromData($model->min_price_excl_vat, $model->min_price_incl_vat))
-            ->setMaxPrice(PriceFactory::fromData($model->max_price_excl_vat, $model->max_price_incl_vat));
+        );
+        if ($model->min_price_excl_vat) {
+            $tariff->setMinPrice(PriceFactory::fromData($model->min_price_excl_vat, $model->min_price_incl_vat));
+        }
+        if ($model->max_price_excl_vat) {
+            $tariff->setMaxPrice(PriceFactory::fromData($model->max_price_excl_vat, $model->max_price_incl_vat));
+        }
+        return $tariff;
     }
 
     /**
