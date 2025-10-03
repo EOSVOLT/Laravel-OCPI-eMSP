4bb039cd1f7b4bb7c5fa7ac32b93cb6e6aaf6586
chanapon <mike0156@hotmail.com>
2025-10-03 16:52:53 +0700

    [EOS-2412] session resource add post functionality

--- src/Modules/Sessions/Client/Resource.php
+++ src/Modules/Sessions/Client/Resource.php
@@ -3,11 +3,31 @@
 namespace Ocpi\Modules\Sessions\Client;
 
 use Ocpi\Support\Client\Resource as OcpiResource;
+use Saloon\Exceptions\Request\FatalRequestException;
+use Saloon\Exceptions\Request\RequestException;
 
 class Resource extends OcpiResource
 {
+    /**
+     * @return array|null
+     * @throws FatalRequestException
+     * @throws RequestException
+     * @throws \Throwable
+     */
     public function all(): ?array
     {
         return $this->requestGetSend();
+    }
+
+    /**
+     * @param array $payload
+     * @return array|null
+     * @throws FatalRequestException
+     * @throws RequestException
+     * @throws \Throwable
+     */
+    public function post(array $payload): ?array
+    {
+        return $this->requestPostSend($payload);
     }
 }
