997c606b47ba3bf729f80b1338d88ca0fce0e022
waritthon thanomsap <wt@eosvolt.com>
2025-10-04 01:40:40 +0700

    update tariff list api

---
+++ src/Modules/Tariffs/Client/V2_2_1/EMSPClient.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Ocpi\Modules\Tariffs\Client\V2_2_1;
+
+use Ocpi\Support\Client\ReceiverClient;
+
+class EMSPClient extends ReceiverClient
+{
+
+}
--- src/Modules/Tariffs/Server/Controllers/CPO/V2_2_1/GetController.php
+++ src/Modules/Tariffs/Server/Controllers/CPO/V2_2_1/GetController.php
@@ -5,6 +5,8 @@ namespace Ocpi\Modules\Tariffs\Server\Controllers\CPO\V2_2_1;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Context;
+use Ocpi\Models\Tariff\Tariff;
+use Ocpi\Modules\Credentials\Object\Party;
 use Ocpi\Modules\Locations\Traits\HandlesLocation;
 use Ocpi\Support\Server\Controllers\Controller;
 use Ocpi\Support\Traits\PageConvertor;
@@ -18,19 +20,23 @@ class GetController extends Controller
      */
     public function list(Request $request): JsonResponse
     {
-        // Minimal implementation: return empty list with OCPI pagination headers
         $offset = (int) $request->input('offset', 0);
         $limit = (int) $request->input('limit', 20);
         $page = self::fromOffset($offset, $limit);
 
-        $data = [];
-        $total = 0;
+        /** @var Party $party */
+        $party = Context::getHidden('party');
+
+        $tariff = Tariff::query()->where('party_id', $party->getId())
+            ->offset($offset)
+            ->limit($limit)
+            ->paginate();
 
         return $this->ocpiSuccessPaginateResponse(
-            $data,
+            $tariff,
             $page,
             $limit,
-            $total,
+            $tariff->total(),
             self::getLocationPath(Context::get('ocpi_version')),
         );
     }
