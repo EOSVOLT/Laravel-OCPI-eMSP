d4948ffe76555b829f74459ea08a7598f495411a
chanapon <mike0156@hotmail.com>
2025-10-15 18:19:50 +0700

    [EOS-2413] update cdr model

---
+++ src/Data/Migrations/2025_10_15_102439_add_location_evse_id.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Ocpi\Models\Locations\LocationEvse;
+use Ocpi\Models\Sessions\Session;
+use Ocpi\Modules\Sessions\Factories\SessionFactory;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table(config('ocpi.database.table.prefix') . 'sessions', function (Blueprint $table) {
+            $table->unsignedBigInteger('location_evse_id')->after('location_id')->nullable();
+            $table->foreign('location_evse_id')->references('id')->on(
+                config('ocpi.database.table.prefix') . 'location_evses'
+            );
+        });
+        //migrate session data
+        Session::all()->each(function (Session $session) {
+            $sessionObj = SessionFactory::fromModel($session);
+            /** @var LocationEvse $evse */
+            $evse = LocationEvse::query()->where('uid', $sessionObj->getSessionDetails()->getEvseUid())->first();
+            $session->location_evse_id = $evse->id;
+            $session->save();
+        });
+        Schema::table(config('ocpi.database.table.prefix') . 'sessions', function (Blueprint $table) {
+            $table->unsignedBigInteger('location_evse_id')->nullable(false)->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table(config('ocpi.database.table.prefix') . 'sessions', function (Blueprint $table) {
+            $table->dropForeign('location_evse_id');
+            $table->dropColumn('location_evse_id');
+        });
+    }
+};
--- src/Models/Cdrs/Cdr.php
+++ src/Models/Cdrs/Cdr.php
@@ -6,10 +6,24 @@ use Illuminate\Database\Eloquent\Casts\AsArrayObject;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Ocpi\Models\Locations\Location;
 use Ocpi\Models\Locations\LocationEvse;
 use Ocpi\Models\PartyRole;
+use Ocpi\Models\Sessions\Session;
 use Ocpi\Support\Models\Model;
 
+/**
+ * @property int $id
+ * @property PartyRole $party_role
+ * @property string $cdr_id
+ * @property array $object
+ * @property Location $location
+ * @property int $location_id
+ * @property LocationEvse $location_evse_id
+ * @property int $location_evse
+ * @property Session $session
+ * @property string $session_id
+ */
 class Cdr extends Model
 {
     use HasUuids,
@@ -19,17 +33,12 @@ class Cdr extends Model
 
     protected $fillable = [
         'party_role_id',
-        'location_evse_emsp_id',
-        'id',
+        'location_id',
+        'location_evse_id',
+        'session_id',
+        'cdr_id',
         'object',
     ];
-
-    protected function casts(): array
-    {
-        return [
-            'object' => AsArrayObject::class,
-        ];
-    }
 
     /***
      * Relations.
@@ -37,11 +46,28 @@ class Cdr extends Model
 
     public function location_evse(): BelongsTo
     {
-        return $this->belongsTo(LocationEvse::class, 'location_evse_emsp_id', 'emsp_id');
+        return $this->belongsTo(LocationEvse::class);
+    }
+
+    public function location(): BelongsTo
+    {
+        return $this->belongsTo(Location::class);
+    }
+
+    public function session(): BelongsTo
+    {
+        return $this->belongsTo(Session::class);
     }
 
     public function party_role(): BelongsTo
     {
         return $this->belongsTo(PartyRole::class);
+    }
+
+    protected function casts(): array
+    {
+        return [
+            'object' => AsArrayObject::class,
+        ];
     }
 }
