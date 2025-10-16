04a79dbc85c804ac934e8caa5c99550d73fac52a
chanapon <mike0156@hotmail.com>
2025-10-16 18:41:13 +0700

    [EOS-2413] refactor for cdr endpoints 3

--- src/Modules/Cdrs/Server/Endpoints/CPO/2.2.1.php
+++ src/Modules/Cdrs/Server/Endpoints/CPO/2.2.1.php
@@ -2,12 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 use Ocpi\Modules\Cdrs\Server\Controllers\CPO\V2_2_1\GetController;
-use Ocpi\Modules\Cdrs\Server\Controllers\CPO\V2_2_1\GetMockedController;
 
 Route::prefix('cdrs')
     ->name('cdrs')
     ->group(function () {
-        Route::get('{cdr_emsp_id?}', GetController::class)->name('.get');
-        // Route only used in Versions details to give an endpoint for this Module to the CPO.
-        Route::get('/', GetMockedController::class);
+        Route::get('/', GetController::class);
     });
