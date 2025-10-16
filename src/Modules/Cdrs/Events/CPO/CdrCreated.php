a5b41c251bea46b388529eb1945302f14551e04c
chanapon <mike0156@hotmail.com>
2025-10-17 01:14:18 +0700

    [EOS-2413] add cdr event readonly attributes

--- src/Modules/Cdrs/Events/CPO/CdrCreated.php
+++ src/Modules/Cdrs/Events/CPO/CdrCreated.php
@@ -9,7 +9,19 @@ class CdrCreated implements ShouldDispatchAfterCommit, SenderCdrEventInterface
 {
     use Dispatchable;
 
-    public function __construct(public int $id)
+    /**
+     * @param int $id
+     */
+    public function __construct(private readonly int $id)
     {
     }
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
 }
