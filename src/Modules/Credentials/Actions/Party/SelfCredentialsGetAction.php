b855bd918cb3390b2ac08e478ac883b0cc8e433f
waritthon thanomsap <wt@eosvolt.com>
2025-10-31 15:33:42 +0700

    handle role in SelfCredentialsGetAction

--- src/Modules/Credentials/Actions/Party/SelfCredentialsGetAction.php
+++ src/Modules/Credentials/Actions/Party/SelfCredentialsGetAction.php
@@ -9,18 +9,17 @@ use Ocpi\Support\Helpers\GeneratorHelper;
 
 class SelfCredentialsGetAction
 {
-    public function handle(Party $party, PartyToken $partyToken): ?array
+    public function handle(PartyRole $partyRole, PartyToken $partyToken): ?array
     {
-        if (null === $party->url) {
+        if (null === $partyRole->url) {
             return null;
         }
-        $token = GeneratorHelper::encodeToken($partyToken->token, $party->version);
-        if (version_compare($party->version, '2.2', '<')) {
-            /** @var PartyRole $role */
-            $role = $party->roles->first();
+        $token = GeneratorHelper::encodeToken($partyToken->token, $partyRole->party->version);
+        if (version_compare($partyRole->party->version, '2.2', '<')) {
+            $role = $partyRole;
 
             return [
-                'url' => $party->url,
+                'url' => $partyRole->url,
                 'token' => $token, //token C
                 'party_id' => $role->code,
                 'country_code' => $role->country_code,
@@ -28,16 +27,14 @@ class SelfCredentialsGetAction
             ];
         } else {
             return [
-                'url' => $party->url,
+                'url' => $partyRole->url,
                 'token' => $token, //token C
-                'roles' => $party->roles->map(function (PartyRole $role) {
-                    return [
-                        'role' => $role->role,
-                        'party_id' => $role->code,
-                        'country_code' => $role->country_code,
-                        'business_details' => $role->business_details,
-                    ];
-                }),
+                'roles' => [
+                    'role' => $partyRole->role,
+                    'party_id' => $partyRole->code,
+                    'country_code' => $partyRole->country_code,
+                    'business_details' => $partyRole->business_details,
+                ],
             ];
         }
     }
