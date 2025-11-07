9beddcdb6e6e9a6903b45541119aad1ba595cc90
chanapon <mike0156@hotmail.com>
2025-11-07 18:26:42 +0700

    [EOS-2934] event dispatch only id

--- src/Modules/Tariffs/Events/EMSP/TariffRemoved.php
+++ src/Modules/Tariffs/Events/EMSP/TariffRemoved.php
@@ -4,18 +4,17 @@ namespace Ocpi\Modules\Tariffs\Events\EMSP;
 
 use Illuminate\Contracts\Events\ShouldDispatchAfterCommit;
 use Illuminate\Foundation\Events\Dispatchable;
-use Ocpi\Modules\Tariffs\Objects\Tariff;
 
 class TariffRemoved implements ShouldDispatchAfterCommit, ReceiverTariffEventInterface
 {
     use Dispatchable;
 
-    public function __construct(private readonly Tariff $tariff)
+    public function __construct(private readonly int $tariffId)
     {
     }
 
-    public function getTariff(): Tariff
+    public function getTariffId(): int
     {
-        return $this->tariff;
+        return $this->tariffId;
     }
 }
