51ec88e5ffa75c954f63de4366fde06b2b0fdf0f
waritthon thanomsap <wt@eosvolt.com>
2025-11-10 18:07:20 +0700

    update migration

--- src/Data/Migrations/2025_11_10_032118_refactor_token_role.php
+++ src/Data/Migrations/2025_11_10_032118_refactor_token_role.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 use Ocpi\Models\Party;
 use Ocpi\Models\PartyRole;
@@ -10,38 +11,69 @@ use Ocpi\Models\PartyToken;
 return new class extends Migration {
     public function up(): void
     {
-        if (!Schema::hasColumn(config('ocpi.database.table.prefix').'party_tokens', 'party_role_id')) {
-            Schema::table(config('ocpi.database.table.prefix').'party_tokens', function (Blueprint $table) {
-                $table->foreignId('party_role_id')->nullable()->after('party_id')->constrained(config('ocpi.database.table.prefix').'party_roles');
-            });
-        }
-        PartyRole::all()->each(function (PartyRole $partyRole) {
-            $currentToken = $partyRole->party->tokens->first();
-            $partyRole->tokens()->create([
-                'party_id' => $currentToken->party_id,
-                'name' => $currentToken->name,
-                'token' => $currentToken->token,
-                'registered' => $currentToken->registered,
-                'deleted_at' => $currentToken->deleted_at
-            ]);
-            $currentToken->delete();
+        Schema::disableForeignKeyConstraints();
+        Schema::table(config('ocpi.database.table.prefix') . 'party_tokens', function (Blueprint $table) {
+            $table->foreignId('party_role_id')->nullable()->after('id')->constrained(
+                config('ocpi.database.table.prefix') . 'party_roles'
+            )->cascadeOnDelete();
         });
+        Party::all()->each(function (Party $party) {
+            $currentTokens = DB::table(config('ocpi.database.table.prefix') . 'party_tokens')
+                ->where('party_id', $party->id)
+                ->whereNull('party_role_id')
+                ->get();
+            if (!$currentTokens) {
+                return;
+            }
+            $party->roles()->each(function (PartyRole $partyRole) use ($party, $currentTokens) {
+                foreach ($currentTokens as $token) {
+                    PartyToken::create([
+                        'party_id' => $party->id,
+                        'party_role_id' => $partyRole->id,
+                        'name' => $token->name,
+                        'token' => $token->token,
+                        'registered' => $token->registered,
+                    ]);
+                }
+            });
+            $currentTokens->each(function ($token) {
+                PartyToken::find($token->id)->forceDelete();
+            });
+        });
+        Schema::table(config('ocpi.database.table.prefix') . 'party_tokens', function (Blueprint $table) {
+            $table->dropConstrainedForeignId('party_id');
+            $table->foreignId('party_role_id')->nullable(false)->change();
+        });
+        Schema::enableForeignKeyConstraints();
     }
 
     public function down(): void
     {
-        PartyRole::all()->each(function (PartyRole $partyRole) {
-            $partyRole->tokens()->get()->each(function (PartyToken $token) {
-                $token->create([
-                    'party_id' => $token->party_id,
-                    'name' => $token->name,
-                    'token' => $token->token,
-                    'registered' => $token->registered,
-                    'deleted_at' => $token->deleted_at,
-                    'party_role_id' => null,
-                ]);
-                $token->delete();
-            });
+        Schema::disableForeignKeyConstraints();
+        Schema::table(config('ocpi.database.table.prefix') . 'party_tokens', function (Blueprint $table) {
+            $table->foreignId('party_id')->nullable()->after('id')->constrained(
+                config('ocpi.database.table.prefix') . 'parties'
+            )->cascadeOnDelete();
         });
+        PartyRole::all()->each(function (PartyRole $partyRole) {
+            PartyToken::where('party_role_id', $partyRole->id)->whereNull('party_id')->get()->each(
+                function (PartyToken $token) use ($partyRole) {
+                    PartyToken::updateOrCreate([
+                        'party_id' => $partyRole->party_id,
+                        'token' => $token->token,
+                    ], [
+                        'party_role_id' => $partyRole->id,
+                        'name' => $token->name,
+                        'registered' => $token->registered,
+                    ]);
+                    $token->forceDelete();
+                }
+            );
+        });
+        Schema::table(config('ocpi.database.table.prefix') . 'party_tokens', function (Blueprint $table) {
+            $table->dropConstrainedForeignId('party_role_id');
+            $table->foreignId('party_id')->nullable(false)->change();
+        });
+        Schema::enableForeignKeyConstraints();
     }
 };
