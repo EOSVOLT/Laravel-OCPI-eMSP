325a6096edce2fd464df0248d1523f081b995f69
chanapon <mike0156@hotmail.com>
2025-11-11 11:52:39 +0700

    [EOS-2804] update test

--- tests/Traits/TestPartyRoleTrait.php
+++ tests/Traits/TestPartyRoleTrait.php
@@ -18,12 +18,12 @@ trait TestPartyRoleTrait
         $tokenMock = Mockery::mock(PartyToken::class, function (MockInterface $mock) {
             $mock->shouldReceive('setAttribute')->andReturnSelf();
             $mock->shouldReceive('getAttribute')->with('id')->andReturn(1);
-            $mock->shouldReceive('getAttribute')->with('party_id')->andReturn(1);
+            $mock->shouldReceive('getAttribute')->with('party_role_id')->andReturn(1);
             $mock->shouldReceive('getAttribute')->with('name')->andReturn('test token');
             $mock->shouldReceive('getAttribute')->with('token')->andReturn(Str::random(32));
             $mock->shouldReceive('getAttribute')->with('registered')->andReturnTrue();
         });
-        $roleMock = Mockery::mock(PartyRole::class, function (MockInterface $mock) {
+        $roleMock = Mockery::mock(PartyRole::class, function (MockInterface $mock) use ($tokenMock) {
             $mock->shouldReceive('setAttribute')->andReturnSelf();
             $mock->shouldReceive('getAttribute')->with('id')->andReturn(1);
             $mock->shouldReceive('getAttribute')->with('party_id')->andReturn(1);
@@ -35,8 +35,11 @@ trait TestPartyRoleTrait
                 'name' => 'Test Business Details',
                 'website' => 'www.test.com',
             ]);
+            $mock->shouldReceive('getAttribute')->with('tokens')->andReturn(new Collection([$tokenMock]));
+            $mock->shouldReceive('getAttribute')->with('url')->andReturn('www.test.com');
+            $mock->shouldReceive('getAttribute')->with('endpoints')->andReturnNull();
         });
-        $partyMock = \Mockery::mock(Party::class, function (MockInterface $mock) use ($tokenMock, $roleMock, $isExternalParty) {
+        $partyMock = \Mockery::mock(Party::class, function (MockInterface $mock) use ($roleMock, $isExternalParty) {
             $mock->shouldReceive('offsetExists')->andReturnTrue();
             $mock->shouldReceive('relationLoaded')->andReturnTrue();
             $mock->shouldReceive('load')->andReturnSelf();
@@ -47,12 +50,9 @@ trait TestPartyRoleTrait
             $mock->shouldReceive('getAttribute')->with('cpo_id')->andReturn(1);
             $mock->shouldReceive('getAttribute')->with('is_external_party')->andReturn($isExternalParty);
             $mock->shouldReceive('getAttribute')->with('code')->andReturn('TH*ABC');
-            $mock->shouldReceive('getAttribute')->with('url')->andReturn('www.test.com');
             $mock->shouldReceive('getAttribute')->with('version')->andReturn('2.2.1');
             $mock->shouldReceive('getAttribute')->with('version_url')->andReturn('test-url.com');
-            $mock->shouldReceive('getAttribute')->with('endpoints')->andReturnNull();
             $mock->shouldReceive('getAttribute')->with('role_cpo')->andReturn($roleMock);
-            $mock->shouldReceive('getAttribute')->with('tokens')->andReturn(new Collection([$tokenMock]));
             $mock->shouldReceive('getAttribute')->with('roles')->andReturn(new Collection([$roleMock]));
         });
 
