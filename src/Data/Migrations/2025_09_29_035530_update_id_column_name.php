dabe99e5eaef0a762949145aef94d30f8eea1f82
chanapon <mike0156@hotmail.com>
2025-11-12 17:51:23 +0700

    [EOS-2804] fix typo migration session

--- src/Data/Migrations/2025_09_29_035530_update_id_column_name.php
+++ src/Data/Migrations/2025_09_29_035530_update_id_column_name.php
@@ -12,52 +12,50 @@ return new class extends Migration {
     public function up(): void
     {
         Schema::enableForeignKeyConstraints();
-        if('sqlite' === DB::connection()->getDriverName()){
+        if ('sqlite' === DB::connection()->getDriverName()) {
             Schema::dropIfExists(config('ocpi.database.table.prefix') . "sessions");
-            Schema::create(config('ocpi.database.table.prefix').'sessions', function (Blueprint $table) {
+            Schema::create(config('ocpi.database.table.prefix') . 'sessions', function (Blueprint $table) {
                 $table->id();
                 $table->foreignId('party_role_id')
                     ->constrained(
-                        table: config('ocpi.database.table.prefix').'party_roles',
+                        table: config('ocpi.database.table.prefix') . 'party_roles',
                         indexName: 'sessions_party_role_id',
                     )
                     ->cascadeOnDelete();
                 $table->foreignUuid('location_evse_emsp_id')
                     ->nullable()
                     ->constrained(
-                        table: config('ocpi.database.table.prefix').'location_evses',
+                        table: config('ocpi.database.table.prefix') . 'location_evses',
                         column: 'emsp_id',
                     )
                     ->cascadeOnDelete();
 
-                $table->string('id', length: 36);
+                $table->string('session_id', length: 36);
                 $table->json('object');
                 $table->timestamps();
                 $table->softDeletes();
 
-                $table->unique(['party_role_id', 'id']);
-                $table->index('id');
+                $table->unique(['party_role_id', 'session_id']);
+                $table->index('session_id');
             });
-        }else{
+        } else {
             Schema::table(config('ocpi.database.table.prefix') . "sessions", function (Blueprint $table) {
+                $table->dropIndex('ocpi_sessions_id_index');
+                $table->dropForeign(['party_role_id']);
+                $table->dropUnique(['party_role_id', 'id']);
+                $table->dropColumn('id');
+                $table->string('session_id', 36)->after('party_role_id');
+                $table->index('session_id');
+                $table->unique(['party_role_id', 'session_id']);
+                $table->foreign('party_role_id')
+                    ->references('id')
+                    ->on(config('ocpi.database.table.prefix') . "party_roles")
+                    ->onDelete('restrict');
                 $table->dropPrimary();
                 $table->renameColumn('emsp_id', 'id');
                 $table->primary('id');
             });
         }
-        Schema::table(config('ocpi.database.table.prefix') . "sessions", function (Blueprint $table) {
-            $table->dropIndex('ocpi_sessions_id_index');
-            $table->dropForeign(['party_role_id']);
-            $table->dropUnique(['party_role_id', 'id']);
-            $table->dropColumn('id');
-            $table->string('session_id', 36)->after('party_role_id');
-            $table->index('session_id');
-            $table->unique(['party_role_id', 'session_id']);
-            $table->foreign('party_role_id')
-                ->references('id')
-                ->on(config('ocpi.database.table.prefix') . "party_roles")
-                ->onDelete('restrict');
-        });
     }
 
     /**
@@ -77,7 +75,7 @@ return new class extends Migration {
             $table->string('id', 36)->after('party_role_id');
             $table->index('id');
             $table->unique(['party_role_id', 'id']);
-            $table->foreign('party_role_id','sessions_party_role_id')
+            $table->foreign('party_role_id', 'sessions_party_role_id')
                 ->references('id')
                 ->on(config('ocpi.database.table.prefix') . "party_roles")
                 ->onDelete('cascade');
