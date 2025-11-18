bc90c510d4029ee2466582cc37729913619cd5d3
waritthon thanomsap <wt@eosvolt.com>
2025-11-18 12:10:35 +0700

    fix event conflict

--- src/Modules/Locations/Events/EMSP/LocationEvseCreated.php
+++ src/Modules/Locations/Events/EMSP/LocationEvseCreated.php
@@ -5,11 +5,10 @@ namespace Ocpi\Modules\Locations\Events\EMSP;
 use Illuminate\Contracts\Events\ShouldDispatchAfterCommit;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Queue\Queueable;
-use Illuminate\Queue\InteractsWithQueue;
 
 class LocationEvseCreated implements ReceiverLocationEventInterface, ShouldDispatchAfterCommit, ShouldQueue
 {
-    use InteractsWithQueue;
+
     use Queueable;
 
     public $connection = 'database';
