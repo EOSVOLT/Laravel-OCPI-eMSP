57958e91d6138250ea2221cd57fbcd26723543bd
chanapon <mike0156@hotmail.com>
2025-12-08 12:52:02 +0700

    [EOS-3047]rename variable and add getters

--- src/Modules/Sessions/Events/EMSP/SessionReplaced.php
+++ src/Modules/Sessions/Events/EMSP/SessionReplaced.php
@@ -9,9 +9,40 @@ class SessionReplaced implements ShouldDispatchAfterCommit, ReceiverSessionEvent
 {
     use Dispatchable;
 
+    /**
+     * @param int $partyRoleId
+     * @param string $id
+     * @param mixed $payload
+     */
     public function __construct(
-        public int $party_role_id,
-        public string $id,
-        public mixed $payload,
-    ) {}
+        private readonly int $partyRoleId,
+        private readonly string $id,
+        private readonly mixed $payload,
+    ) {
+    }
+
+    /**
+     * @return int
+     */
+    public function getPartyRoleId(): int
+    {
+        return $this->partyRoleId;
+    }
+
+    /**
+     * @return string
+     */
+    public function getId(): string
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPayload(): mixed
+    {
+        return $this->payload;
+    }
+
 }
