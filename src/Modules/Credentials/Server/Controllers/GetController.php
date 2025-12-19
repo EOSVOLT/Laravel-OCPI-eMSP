475397542a224c258f19cd8db699dd92c1d45422
chanapon <mike0156@hotmail.com>
2025-12-19 15:38:24 +0700

    hotfix, update get credential

--- src/Modules/Credentials/Server/Controllers/GetController.php
+++ src/Modules/Credentials/Server/Controllers/GetController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Context;
 use Ocpi\Models\Party;
+use Ocpi\Models\PartyToken;
 use Ocpi\Modules\Credentials\Actions\Party\SelfCredentialsGetAction;
 use Ocpi\Support\Enums\OcpiServerErrorCode;
 use Ocpi\Support\Server\Controllers\Controller;
@@ -14,14 +15,15 @@ class GetController extends Controller
 {
     public function __invoke(Request $request, SelfCredentialsGetAction $selfCredentialsGetAction): JsonResponse
     {
-        $party = Party::where('code', Context::get('party_code'))->first();
+        $parentToken = PartyToken::query()->with(['party_role.party'])->find(Context::get('token_id'));
+        $party = $parentToken->party_role->party;
         if ($party === null) {
             return $this->ocpiServerErrorResponse(
                 statusCode: OcpiServerErrorCode::PartyApiUnusable
             );
         }
 
-        $data = $selfCredentialsGetAction->handle($party);
+        $data = $selfCredentialsGetAction->handle($parentToken);
 
         return $data
             ? $this->ocpiSuccessResponse($data)
