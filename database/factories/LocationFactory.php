ac414f0a57f38ac4bcf35560c886395af97a6d95
chanapon <mike0156@hotmail.com>
2026-01-06 15:21:31 +0700

    [EOS-3162] fix default location party factories

--- database/factories/LocationFactory.php
+++ database/factories/LocationFactory.php
@@ -5,6 +5,7 @@ namespace Ocpi\Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Ocpi\Models\Locations\Location;
 use Ocpi\Models\Party;
+use Ocpi\Models\PartyRole;
 use Ocpi\Tests\Traits\SchemaTrait;
 
 class LocationFactory extends Factory
@@ -15,8 +16,10 @@ class LocationFactory extends Factory
 
     public function definition(): array
     {
+        /** @var PartyRole $partyRole */
+        $partyRole = PartyRole::factory()->create();
         return [
-            'party_id' => Party::factory(),
+            'party_id' => $partyRole->party_id,
             'external_id' => $this->faker->uuid,
             'object' => $this->getJsonResourceArray(__DIR__ . "/../../src/Tests/Resources/location_object.json"),
             'publish' => true,
