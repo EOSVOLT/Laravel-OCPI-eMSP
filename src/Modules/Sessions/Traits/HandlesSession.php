0d7617aef400b99cd4c7cc088dc37ad770142879
waritthon thanomsap <wt@eosvolt.com>
2026-01-11 21:13:51 +0700

    fix stopSession

--- src/Modules/Sessions/Traits/HandlesSession.php
+++ src/Modules/Sessions/Traits/HandlesSession.php
@@ -136,7 +136,7 @@ trait HandlesSession
 
     private function stopSession(array $payload, Session $session): bool
     {
-        $session->status = $status ?? $session->status;
+        $session->status = SessionStatus::tryFrom($payload['status'] ?? '') ?? $session->status;
         $object = $session->object ?? [];
         foreach ($payload as $field => $value) {
             $object[$field] = $value;
